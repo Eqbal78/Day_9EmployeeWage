8dba73a3c997b420b5f22a30bde97bc2dad518b7
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-08-21 09:54:41 +0530

    Add partTime employee wage

---
+++ UC-3_PartTimeEmployee.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+echo "Welcome to Employee Wage Computation"
+
+Attendance=$((RANDOM%2))
+
+if [ $Attendance -eq 1 ]
+then
+    echo "Employee is Present"
+else
+    echo "Employee is Absent"
+fi
+
+isPartTime=0
+isFullTime=1
+empRatePerHr=20
+
+if [[ $isFullTime -eq $Attendance ]]
+then
+    empHrs=8;
+else
+    empHrs=0;
+    empHrs=4;
+fi
+
+echo "dailyWage=$(($empHrs*$empRatePerHr))"
