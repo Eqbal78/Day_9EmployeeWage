7d75778fd6fbc9b36caf5f2d8fc6dc17dc3c1624
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-08-21 09:30:07 +0530

    Calculate Employee Daily Wage

---
+++ UC-2_EmployeeDailyWage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation"
+
+Attendance=$((RANDOM%2))
+
+if [ $Attendance -eq 1 ]
+then
+    echo "Employee is Present"
+else
+    echo "Employee is Absent"
+fi
+
+isFullTime=1
+empRatePerHr=20
+
+empCheck=$((RANDOM%2))
+
+if [ $isFullTime -eq $empCheck ]
+then
+    empHrs=8;
+else
+    empHrs=0;
+fi
+
+echo "dailyWage=$(($empHrs*$empRatePerHr))"
+
